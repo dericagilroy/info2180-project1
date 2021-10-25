31e49337e7220ac16721b3b8258ef016924bbeef
dericagilroy <dericagilroy24@gmail.com>
2021-10-24 21:51:30 -0500

    Edited scripts.js; added an event listener to the "Subscribe" button that displays a message when an email has been or has not been entered

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
-/*Add your JS to this file*/
+"use strict";
+
+window.addEventListener("DOMContentLoaded", function() {subscribe()});
+
+function subscribe() {
+    document.getElementsByClassName("btn")[1].addEventListener("click", authenticate);
+}
+
+function authenticate(event) {
+    event.preventDefault();
+
+    if (document.getElementById("email").value == "") {
+        document.getElementsByClassName("message")[0].textContent = "Please enter a valid email address.";
+    }
+    else {
+        document.getElementsByClassName("message")[0].textContent = "Thank you! Your email address <" + document.getElementById("email").value + "> has been added to our mailing list!";
+    }
+}
